00b466b31fa7d54a25c793fccab103e94a4899fc
Metalxiaoxiao <58896585+Metalxiaoxiao@users.noreply.github.com>
2025-07-18 10:47:13 +0800

    UPDATE

--- src/login.ts
+++ src/login.ts
@@ -1,23 +1,12 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 @customElement('login-form')
 export class LoginForm extends LitElement {
-  /**
-   * Copy for the read the docs hint.
-   */
+
   @property()
   docsHint = 'Click on the Vite and Lit logos to learn more'
 
-  /**
-   * The number of times the button has been clicked.
-   */
   @property({ type: Number })
   count = 0
   createRenderRoot() {
