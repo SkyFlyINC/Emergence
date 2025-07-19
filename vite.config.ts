1e5a92155a925ead26921dadffc28a8bb31c10cf
Bell-xyz <160737039+Bell-xyz@users.noreply.github.com>
2025-07-19 16:34:12 +0800

    用户界面更新
    AAAAA王哥建材

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-  plugins: [
+  plugins: [ 
+    
     tailwindcss(),
-  ],
+    
+  ], build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
 })
